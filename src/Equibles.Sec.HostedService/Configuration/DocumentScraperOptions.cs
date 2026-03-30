1f36c0da8a11c2257f9fcd3d0865932914604d28
Daniel Oliveira <daniel-oliveira-11@hotmail.com>
2026-03-30 01:24:33 +0100

    Revert "fix(Sec): use strings in DocumentScraperOptions for config binding"
    This reverts commit 9cd56e1ae9ee728a2573582b3dd76b473d0b9a2f.

--- src/Equibles.Sec.HostedService/Configuration/DocumentScraperOptions.cs
+++ src/Equibles.Sec.HostedService/Configuration/DocumentScraperOptions.cs
@@ -3,16 +3,11 @@ using Equibles.Sec.Data.Models;
 namespace Equibles.Sec.HostedService.Configuration;
 
 public class DocumentScraperOptions {
-    public List<string> DocumentTypesToSync { get; set; } = [
-        "TenK", "TenQ", "EightK", "FormFour", "FormThree"
+    public List<DocumentType> DocumentTypesToSync { get; set; } = [
+        DocumentType.TenK,
+        DocumentType.TenQ,
+        DocumentType.EightK,
+        DocumentType.FormFour,
+        DocumentType.FormThree
     ];
-
-    private List<DocumentType> _resolvedTypes;
-
-    public List<DocumentType> GetDocumentTypes() {
-        return _resolvedTypes ??= DocumentTypesToSync
-            .Select(DocumentType.FromValue)
-            .Where(t => t != null)
-            .ToList();
-    }
 }
