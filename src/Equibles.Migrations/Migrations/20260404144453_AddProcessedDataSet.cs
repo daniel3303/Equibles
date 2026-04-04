aa4cb0ade68af97e67526120a9f917fa61b2fcd5
Daniel Oliveira <daniel-oliveira-11@hotmail.com>
2026-04-04 15:45:54 +0100

    build: add ProcessedDataSet migration

---
+++ src/Equibles.Migrations/Migrations/20260404144453_AddProcessedDataSet.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Equibles.Migrations.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddProcessedDataSet : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ProcessedDataSet",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    FileName = table.Column<string>(type: "character varying(128)", maxLength: 128, nullable: false),
+                    SubmissionCount = table.Column<int>(type: "integer", nullable: false),
+                    CreationTime = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ProcessedDataSet", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ProcessedDataSet_FileName",
+                table: "ProcessedDataSet",
+                column: "FileName",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ProcessedDataSet");
+        }
+    }
+}
