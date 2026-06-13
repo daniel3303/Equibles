5cd3a10926b9394ad47d185a309a1fc48447428e
Daniel Oliveira <danieloliveira3303@gmail.com>
2026-06-13 01:07:22 +0100

    fix(sec): index Document.CreationTime for ingestion-stats range scans

---
+++ src/Equibles.Migrations/Migrations/20260613000553_AddDocumentCreationTimeIndex.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Equibles.Migrations.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddDocumentCreationTimeIndex : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Document_CreationTime",
+                table: "Document",
+                column: "CreationTime");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Document_CreationTime",
+                table: "Document");
+        }
+    }
+}
